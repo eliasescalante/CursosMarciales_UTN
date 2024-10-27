b6cab0b2d1501526eb51214487e540a24147845c
eliasescalante <eliasw.escalante@gmail.com>
2024-10-27 19:57:49 -0300

    agrego funcion al admin

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Curso  # Asegúrate de importar tu modelo
 
-# Register your models here.
+class CursoAdmin(admin.ModelAdmin):
+    # Puedes definir qué campos se mostrarán en el formulario de creación/edición
+    fieldsets = [
+        ("Información del Curso", {"fields": ["nombre", "comision", "Profesor", "Descripcion", "cupo", "precio", "imagen", "direccion"]}),
+    ]
+    
+    # Campos que se mostrarán en la lista de cursos en la vista admin
+    list_display = ['nombre', 'comision', 'Profesor', 'precio', 'cupo']
+    
+    # Permitir filtrado y búsqueda
+    list_filter = ('comision', 'Profesor')
+    search_fields = ('nombre', 'Profesor')
+    ordering = ['-cupo']  # Puedes cambiar el orden según tus necesidades
+
+# Registra el modelo y la clase de administración
+admin.site.register(Curso, CursoAdmin)
