55926d8498f0d85a27d3ad491b21e2fe3dbbe5a9
eliasescalante <eliasw.escalante@gmail.com>
2024-11-24 15:06:57 -0300

    agrego vista mis cursos y la posibilidad de inscribirme usando un ticket

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('home/', views.home_cursos, name='home_cursos'),
     path('noticias/', views.noticias, name='noticias'),
     path('mis-cursos/', views.mis_cursos, name='mis_cursos'),
+    path('inscribirse/<int:curso_id>/', views.inscribirse_curso, name='inscribirse_curso'),
+    path('detalle/<int:curso_id>/', views.detalle_curso, name='detalle_curso')
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,7 @@
-from .models import Curso
-from django.shortcuts import render
+from .models import Curso, Ticket
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 def home_cursos(request):
     cursos = Curso.objects.all()  # O la consulta que necesites
@@ -9,5 +11,43 @@ def noticias(request):
     return render(request, 'cursos/noticias.html')
 
 # Vista para la página de mis cursos
+"""
 def mis_cursos(request):
     return render(request, 'cursos/mis_cursos.html')
+"""
+
+@login_required
+def inscribirse_curso(request, curso_id):
+    curso = get_object_or_404(Curso, id=curso_id)
+    usuario = request.user
+
+    # Verificar si ya está inscrito
+    if Ticket.objects.filter(usuario=usuario, curso=curso).exists():
+        messages.warning(request, f'Ya estás inscrito en el curso "{curso.nombre}".')
+        return redirect('detalle_curso', curso_id=curso.id)
+
+    # Verificar si hay cupo
+    if curso.cupo <= 0:
+        messages.error(request, f'El curso "{curso.nombre}" no tiene cupos disponibles.')
+        return redirect('detalle_curso', curso_id=curso.id)
+
+    # Crear el ticket y reducir el cupo
+    Ticket.objects.create(usuario=usuario, curso=curso, estado='pendiente')
+    curso.cupo -= 1
+    curso.save()
+    messages.success(request, f'Te has inscrito en el curso "{curso.nombre}" con éxito.')
+    return redirect('mis_cursos')
+
+@login_required
+def mis_cursos(request):
+    # Recuperar todos los tickets del usuario logueado
+    tickets = Ticket.objects.filter(usuario=request.user).select_related('curso')
+
+    # Extraer los cursos de los tickets
+    cursos = [ticket.curso for ticket in tickets]
+
+    return render(request, 'cursos/mis_cursos.html', {'cursos': cursos})
+
+def detalle_curso(request, curso_id):
+    curso = get_object_or_404(Curso, id=curso_id)
+    return render(request, 'cursos/detalle_curso.html', {'curso': curso})
