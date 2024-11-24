80157d797d43a0db39e3c9a0fbfcf64aba33767c
eliasescalante <eliasw.escalante@gmail.com>
2024-11-24 12:39:04 -0300

    agrego señal cuando se actuliza el perfil

--- usuarios/signals.py
+++ usuarios/signals.py
@@ -8,3 +8,9 @@ def crear_perfil(sender, instance, created, **kwargs):
     if created:
         Perfil.objects.create(user=instance)
         print("Se han creado un perfil para el usuario")
+
+@receiver(post_save, sender=Perfil)
+def perfil_actualizado(sender, instance, created, **kwargs):
+    if not created:
+        # Si el perfil ya existe (es actualizado), se emite esta señal
+        print(f"El perfil de {instance.user.username} ha sido actualizado.")
